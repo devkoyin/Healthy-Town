43a9d2853713fecf7982c23335a4dcfd681822fd
Omolara-Aworeni <omolaramary735@gmail.com>
2026-05-14 14:04:55 +0100

    feat: enable browser animations for Material dialog

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { FoodService } from './services/food/food.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), FoodService]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    FoodService,
+  ],
 };
